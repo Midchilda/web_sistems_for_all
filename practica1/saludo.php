cd2eac5f4a3325a612198cc80f4b96b34b404ad9
Midchilda <jhycopara@ucm.es>
2017-02-27 00:14:39 +0100

    Initial commit

---
+++ practica1/saludo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+	<head> 
+		<title>PHP</title>
+	</head>
+	<body>
+		
+
+		<?php
+			$vueltas = $_GET['num']; //Para leer el parámetro que venía en la dirección (e.g. ?num=50)
+			echo '<h1>Me has pedido que te salude '.$vueltas.' veces </h1>';
+			for ($i = 0; $i < $vueltas; $i++) {
+				echo '<p>' . $i . ' - ¡Hola Mundo!</p>';
+			}
+		?>
+	</body>
+</html>
